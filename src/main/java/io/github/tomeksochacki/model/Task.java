070ebfb79d107cf4fa2c9574ae3c0587e5659be7
tomek <tomeksochacki@wp.pl>
2020-08-05 12:30:34 +0200

    Initialize repository

---
+++ src/main/java/io/github/tomeksochacki/model/Task.java
@@ -0,0 +1,42 @@
+package io.github.tomeksochacki.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message ="Task's description must not be empty")
+    private String description;
+    private boolean done;
+
+    Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+}
