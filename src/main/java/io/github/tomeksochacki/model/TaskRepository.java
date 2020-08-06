f01f9279160f7a03b600479eba2a78e1bb330178
tomek <tomeksochacki@wp.pl>
2020-08-06 16:01:23 +0200

    Update

--- src/main/java/io/github/tomeksochacki/model/TaskRepository.java
+++ src/main/java/io/github/tomeksochacki/model/TaskRepository.java
@@ -1,8 +1,16 @@
 package io.github.tomeksochacki.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
-interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    @RestResource(path = "done", rel = "done")
+    List<Task> findByDone(@Param("state") boolean done);
 }
